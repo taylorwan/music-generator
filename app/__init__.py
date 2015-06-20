202f2b71a2ce209bb3000db9173d81730510708f
taylor <taylor@splashthat.com>
2015-06-20 17:15:35 -0400

    working endpoint for songname/artist

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/',methods=['GET','POST'])
-def show_index():
-    return redirect_url('.onsets/'+songname)
-
-@app.route('/onsets/<songname>',methods=['GET'])
-def show_index():
-    return 'hello world'
+@app.route('/onsets/<songname>/<artist>', methods=['GET'])
+def play_song(songname, artist):
+    return ('%s, %s' % (songname,artist))
 
 if __name__ == '__main__':
     app.run()
