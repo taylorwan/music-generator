6d1997a12c322a0856e90f6d397dc612915637c8
Sergio Oramas Martin <Sergio@MacBook-Pro-de-Sergio-2.local>
2015-06-20 19:36:07 -0400

    freesound downloader

---
+++ get_sounds.py
@@ -0,0 +1,20 @@
+import urllib2, urllib
+import json
+import sys
+import os
+
+token = os.environ["FREESOUND_TOKEN"]
+word = str(sys.argv[1])
+print word
+url = "http://www.freesound.org/apiv2/search/text/?query="+word+"&token="+token+"&filter=duration:[*%20TO%204]"
+response = urllib2.urlopen(url)
+sounds = json.load(response)
+for i in range(0,8):
+	id = sounds['results'][i]['id']
+	url = "http://www.freesound.org/apiv2/sounds/%s/?token=4c634403c33afe15b13bdb8d9925bf33fa1fb702&" % id
+	response = urllib2.urlopen(url)
+	sound = json.load(response)
+	file = sound['previews']['preview-lq-mp3']
+	testfile = urllib.URLopener()
+	testfile.retrieve(file,str(i)+".mp3")
+
